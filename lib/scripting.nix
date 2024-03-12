23a4ed6bea8e74a3944e6b67c431001897df6917
Simon Théorêt <simonteoret@hotmail.com>
2024-03-12 01:14:49 -0400

    updated lib + started scripting

--- lib/scripting.nix
+++ lib/scripting.nix
@@ -1,16 +1,16 @@
-{ pkgs, stdenv, ... }: {
+{ pkgs, stdenv, ... }: rec {
   # Similar to writeShellScript and writeScriptBin.
   # Writes an executable Shell script to /nix/store/<store path>/bin/<name> and checks its syntax.
-  # Automatically includes interpreter above the contents passed.
+  # Automatically includes interpreter with the shellstring argument above the contents passed.
   #
   # Example:
-  # # Writes my-file to /nix/store/<store path>/bin/my-file and makes executable.
-  # writeShellScriptBin "my-file"
+  # # Writes my-file to /nix/store/<store path>/bin/my-file and makes executable with bash shebang.
+  # writeShellScriptBin "my-file" pkgs.${python}
   #   ''
   #   Contents of File
   #   '';
   #
-  writeShellScriptBinBang = name: text: shellstring:
+  writeShellScriptBinBang = name: shellstring: text:
     pkgs.writeTextFile {
       inherit name;
       executable = true;
@@ -23,4 +23,7 @@
         ${stdenv.shell} -n $out/bin/${name}
       '';
     };
+
+  writeShellScriptNix = name: text:
+    writeShellScriptBinBang { shellstring = "/usr/bin/env nix-shell"; };
 }

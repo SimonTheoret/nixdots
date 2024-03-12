8d249648c7c3cc0e04bed42671652aec734623c8
Simon Théorêt <simonteoret@hotmail.com>
2024-03-12 00:53:44 -0400

    added cute function to library for shell scripts

--- lib/scripting.nix
+++ lib/scripting.nix
@@ -1,2 +1,26 @@
-{ pkgs }: {
+{ pkgs, stdenv, ... }: {
+  # Similar to writeShellScript and writeScriptBin.
+  # Writes an executable Shell script to /nix/store/<store path>/bin/<name> and checks its syntax.
+  # Automatically includes interpreter above the contents passed.
+  #
+  # Example:
+  # # Writes my-file to /nix/store/<store path>/bin/my-file and makes executable.
+  # writeShellScriptBin "my-file"
+  #   ''
+  #   Contents of File
+  #   '';
+  #
+  writeShellScriptBinBang = name: text: shellstring:
+    pkgs.writeTextFile {
+      inherit name;
+      executable = true;
+      destination = "/bin/${name}";
+      text = ''
+        #!${shellstring}
+        ${text}
+      '';
+      checkPhase = ''
+        ${stdenv.shell} -n $out/bin/${name}
+      '';
+    };
 }

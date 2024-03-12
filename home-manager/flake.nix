e4a4c0d7d5a3913968cc6608b713490211fd8d51
Simon Théorêt <simonteoret@hotmail.com>
2024-03-12 00:02:01 -0400

    started nix project in home-manager

--- home-manager/flake.nix
+++ home-manager/flake.nix
@@ -10,64 +10,79 @@
 
   outputs = { self, home-manager, nixpkgs, utils }:
     let
-      pkgsForSystem = system: import nixpkgs {
-        inherit system;
-        config.allowUnfree = true;
-      };
 
-      mkHomeConfiguration = args: home-manager.lib.homeManagerConfiguration (rec {
-        modules = [ (import ./homenix/home.nix) ] ++ (args.modules or []);
-        pkgs = pkgsForSystem (args.system or "x86_64-linux");
-      } // { inherit (args) extraSpecialArgs; });
+      supportedSystems =
+        [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forEachSupportedSystem = f:
+        nixpkgs.lib.genAttrs supportedSystems
+        (system: f { pkgs = import nixpkgs { inherit system; }; });
+      pkgsForSystem = system:
+        import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        };
 
-    in utils.lib.eachSystem [ "x86_64-linux" "aarch64-darwin" "x86_64-darwin" ] (system: rec {
-      legacyPackages = pkgsForSystem system;
-  }) // {
-    # non-system suffixed items should go here
-    nixosModules.home = import ./homenix/home.nix; # attr set or list
+      mkHomeConfiguration = args:
+        home-manager.lib.homeManagerConfiguration (rec {
+          modules = [ (import ./homenix/home.nix) ] ++ (args.modules or [ ]);
+          pkgs = pkgsForSystem (args.system or "x86_64-linux");
+        } // {
+          inherit (args) extraSpecialArgs;
+        });
 
-    homeConfigurations.simon = mkHomeConfiguration {
-      extraSpecialArgs = {};
+    in utils.lib.eachSystem [ "x86_64-linux" "aarch64-darwin" "x86_64-darwin" ]
+    (system: rec { legacyPackages = pkgsForSystem system; }) // {
+      #
+
+      devShells = forEachSupportedSystem ({ pkgs }: {
+        default = pkgs.mkShell { packages = with pkgs; [ nil nixfmt ]; };
+      });
+
+      #
+      # non-system suffixed items should go here
+      nixosModules.home = import ./homenix/home.nix; # attr set or list
+
+      homeConfigurations.simon =
+        mkHomeConfiguration { extraSpecialArgs = { }; };
+
+      # homeConfigurations.server = mkHomeConfiguration {
+      #   extraSpecialArgs = {
+      #     withGUI = false;
+      #     isDesktop = false;
+      #     networkInterface = "enp68s0";
+      #   };
+      # };
+      #
+      # homeConfigurations.laptop = mkHomeConfiguration {
+      #   extraSpecialArgs = {
+      #     withGUI = true;
+      #     isDesktop = true;
+      #     networkInterface = "wlp1s0";
+      #   };
+      # };
+      #
+      # homeConfigurations.work = mkHomeConfiguration {
+      #   modules = [ ({lib,...}:
+      #   {
+      #     home.username = lib.mkForce "jringer";
+      #     home.homeDirectory = lib.mkForce "/home/jringer";
+      #   })];
+      #   extraSpecialArgs = {
+      #     withGUI = true;
+      #     isDesktop = true;
+      #     networkInterface = "wlp1s0";
+      #   };
+      # };
+      #
+      # homeConfigurations.mac-mini = mkHomeConfiguration {
+      #   extraSpecialArgs = {
+      #     withGUI = false;
+      #     isDesktop = false;
+      #     networkInterface = "en1";
+      #   };
+      # };
+
+      inherit home-manager;
+      inherit (home-manager) packages;
     };
-
-    # homeConfigurations.server = mkHomeConfiguration {
-    #   extraSpecialArgs = {
-    #     withGUI = false;
-    #     isDesktop = false;
-    #     networkInterface = "enp68s0";
-    #   };
-    # };
-    #
-    # homeConfigurations.laptop = mkHomeConfiguration {
-    #   extraSpecialArgs = {
-    #     withGUI = true;
-    #     isDesktop = true;
-    #     networkInterface = "wlp1s0";
-    #   };
-    # };
-    #
-    # homeConfigurations.work = mkHomeConfiguration {
-    #   modules = [ ({lib,...}:
-    #   {
-    #     home.username = lib.mkForce "jringer";
-    #     home.homeDirectory = lib.mkForce "/home/jringer";
-    #   })];
-    #   extraSpecialArgs = {
-    #     withGUI = true;
-    #     isDesktop = true;
-    #     networkInterface = "wlp1s0";
-    #   };
-    # };
-    #
-    # homeConfigurations.mac-mini = mkHomeConfiguration {
-    #   extraSpecialArgs = {
-    #     withGUI = false;
-    #     isDesktop = false;
-    #     networkInterface = "en1";
-    #   };
-    # };
-
-    inherit home-manager;
-    inherit (home-manager) packages;
-  };
 }

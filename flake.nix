1c4e05a0568b107453165c99826c93dd237c7ef4
Simon Théorêt <simonteoret@hotmail.com>
2024-03-15 13:21:40 -0400

    added lua ls for dotfiles flake

--- flake.nix
+++ flake.nix
@@ -96,7 +96,7 @@
 
       # shell config part 2 starts here
       devShells = forEachSupportedSystem ({ pkgs }: {
-        default = pkgs.mkShell { packages = with pkgs; [ nil nixfmt ]; };
+        default = pkgs.mkShell { packages = with pkgs; [ nil nixfmt lua lua-language-server ]; };
       });
       # shell config part 2 ends here
     };

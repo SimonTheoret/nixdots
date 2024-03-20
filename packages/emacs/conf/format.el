fec2fbd7931532bf03816f365fdcde2708cfaf64
Simon Théorêt <simonteoret@hotmail.com>
2024-03-20 11:51:55 -0400

    bye bye apheialfeiohfa, bonjour format-all

--- packages/emacs/conf/format.el
+++ packages/emacs/conf/format.el
@@ -1,14 +1,14 @@
 ;; -*- lexical-binding: t -*- 
 
-(use-package format-all
-  :commands format-all-mode
-  :hook (prog-mode . format-all-mode)
-  :config
-  (setq-default format-all-formatters
-                '(("Rust"     (rustfmt))
-                  ("Shell" (shfmt "-i" "4" "-ci"))
-		  ("Go" (gofumpt))
-		  ("Python" (ruff "format"))
-		  ))
-  
-  )
+(use-package
+ format-all
+ :commands format-all-mode
+ :hook (prog-mode . format-all-mode)
+ :config
+ (setq-default format-all-formatters
+               '(("Rust" (rustfmt))
+                 ("Shell" (shfmt "-i" "4" "-ci"))
+                 ("Go" (gofumpt))
+                 ("Python" (ruff "format"))
+		 ("Nix" (nixfmt))
+		 ("Lua" (lua-fmt)))))

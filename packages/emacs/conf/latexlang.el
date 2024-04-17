a2010bbcf8c04af132a6ec8dd2a44e872d3a305c
Simon Théorêt <simonteoret@hotmail.com>
2024-04-17 16:28:42 -0400

    Latex+emacs is great

--- packages/emacs/conf/latexlang.el
+++ packages/emacs/conf/latexlang.el
@@ -1,18 +1,31 @@
-;; -*- lexical-binding: t -*- 
+;; -*- lexical-binding: t -*-
 
 (use-package tex
   :straight auctex
   :config
-  (setq TeX-auto-save t)
-  (setq TeX-parse-self t)
   (setq-default TeX-master nil)
-  (setq TeX-PDF-mode t)
-  (setq TeX-electric-sub-and-superscript t)
+  (setq TeX-auto-save t
+	TeX-parse-self t
+	TeX-PDF-mode t
+	TeX-electric-sub-and-superscript t
+	TeX-view-program-selection '((output-pdf "PDF Tools"))
+	TeX-source-correlate-start-server t)
+  (add-to-list 'TeX-command-list '("latexmk" "latexmk -pdf -pvc %t" TeX-run-TeX))
   :hook
   ( LaTeX-mode . visual-line-mode)
   ( LaTeX-mode . flyspell-mode)
   ( LaTeX-mode . LaTeX-math-mode)
-  ;; () 
+  ( TeX-after-compilation-finished-functions . TeX-revert-document-buffer)
+  ;; ()
+  :general-config
+  (general-def
+    :states 'normal
+    :keymaps 'LaTeX-mode-map
+    :prefix "<localleader>"
+    :prefix-command 'Local
+    "m"
+    '("Latex master" . TeX-command-master)
+    )
   )
 
 ;; (use-package auctex-latexmk
@@ -28,3 +41,10 @@
   :hook
   (LaTeX-mode . cdlatex-mode)
   (org-mode . org-cdlatex-mode))
+
+;; (general-def
+;;   :states 'normal
+;;   :keymaps '(latex-mode-map)
+;;   :prefix "<localleader>"
+;;   :prefix-command 'Local
+;;   )

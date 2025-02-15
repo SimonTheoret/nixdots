b194bff431873c2de93e5f5c8526d4995326c345
Simon Theoret <simonteoret@hotmail.com>
2025-02-15 13:37:07 -0500

    Removed deprecated option

--- modules/bluetooth.nix
+++ modules/bluetooth.nix
@@ -12,18 +12,12 @@ in
       example = true;
       description = "Activate bluetooth";
     };
-    guiControls = pkgs.lib.mkOption {
-      type = pkgs.lib.types.bool;
-      default = false;
-      example = true;
-      description = "Use blueman-manager in a GUI to configure bluetooth";
-    };
   };
 
   config = mkIf cfg.enable {
     hardware.bluetooth.enable = true; # enables support for Bluetooth
     hardware.bluetooth.powerOnBoot = true; # powers up the default Bluetooth controller on boot
-    services.blueman.enable = config.myUi.useGUI; # enables gui with blueman
+    services.blueman.enable = true; # enables gui with blueman
     # services.mpris-proxy.enable = true; # enable headsets buttons #Move that to HM?
   };
 }

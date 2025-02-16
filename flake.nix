6f38052231376bf61df88172b8f7a84b9dcd67b4
Simon Theoret <simonteoret@hotmail.com>
2025-02-16 16:07:02 -0500

    Included flake-utils and added a default shell

--- flake.nix
+++ flake.nix
@@ -3,26 +3,47 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, ... }@inputs:
-  let
-	inherit (self) outputs;
-  in
-  {
-    nixosConfigurations.desktop = nixpkgs.lib.nixosSystem {
-      specialArgs = {inherit inputs outputs; userName="simon";};
-      modules = [
-	      ./profiles/desktop.nix
-      ];
-    };
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      ...
+    }@inputs:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        inherit (self) outputs;
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        nixosConfigurations.desktop = nixpkgs.lib.nixosSystem {
+          specialArgs = {
+            inherit inputs outputs;
+            userName = "simon";
+          };
+          modules = [
+            ./profiles/desktop.nix
+          ];
+        };
 
-    nixosConfigurations.laptop = nixpkgs.lib.nixosSystem {
-      specialArgs = {inherit inputs outputs; userName="simon";};
-      modules = [
-	      ./profiles/laptop.nix
-      ];
-    };
+        nixosConfigurations.laptop = nixpkgs.lib.nixosSystem {
+          specialArgs = {
+            inherit inputs outputs;
+            userName = "simon";
+          };
+          modules = [
+            ./profiles/laptop.nix
+          ];
+        };
 
-  };
+        devShells.default = pkgs.mkShell {
+          name = "Nixdots Shell";
+          buildInputs = with pkgs; [ ];
+        };
+      }
+    );
 }

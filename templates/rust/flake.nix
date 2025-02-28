938b84470b796b4104bdaaed3e3db5cfb4ff7154
Simon Theoret <simonteoret@hotmail.com>
2025-02-28 16:46:42 -0500

    Modified rust flake

--- templates/rust/flake.nix
+++ templates/rust/flake.nix
@@ -2,34 +2,36 @@
   description = "A rust development shell";
 
   inputs = {
-    nixpkgs.url      = "github:NixOS/nixpkgs/nixos-unstable";
-    rust-overlay.url = "github:oxalica/rust-overlay";
-    flake-utils.url  = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
-        overlays = [ (import rust-overlay) ];
+        overlays = [ ];
         pkgs = import nixpkgs {
           inherit system overlays;
         };
-        rust = pkgs.rust-bin.stable.latest.default.override {
-              extensions = ["rust-src" "rust-analyzer" "clippy"];
-              targets = [];
-            };
       in
       {
         devShells.default = pkgs.mkShell {
-          buildInputs = [rust] ++ (with pkgs;[
-            pkg-config
-            nixfmt
-            nil
-            shellcheck
-            shfmt
-            nodePackages_latest.bash-language-server
-            cargo-expand
-          ]);
+          buildInputs = (
+            with pkgs;
+            [
+              cargo
+              rustfmt
+              rust-analyzer
+              cargo-expand
+            ]
+          );
         };
       }
     );

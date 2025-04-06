f8914c451c78e87cfedc055fb4f8c02abfacb8f2
Simon Théorêt <simonteoret@hotmail.com>
2025-04-05 22:39:30 -0400

    doc: added documentation

--- modules/commons.nix
+++ modules/commons.nix
@@ -50,8 +50,6 @@ in
     services.syncthing = {
       enable = true;
       openDefaultPorts = true;
-      # Optional: GUI credentials (can be set in the browser instead if you don't want plaintext credentials in your configuration.nix file)
-      # or the password hash can be generated with "syncthing generate --config <path> --gui-password=<password>"
       settings.gui = {
         user = "simon";
         password = "admin";

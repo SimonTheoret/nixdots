1418c7fd5d9dc1248e2cd421412fca207866a336
Simon Théorêt <simonteoret@hotmail.com>
2026-07-07 21:29:10 -0400

    fix(nix): use right derivation for wallpaper

--- packages/wallpaper/wallpaper.nix
+++ packages/wallpaper/wallpaper.nix
@@ -1,21 +1,18 @@
 {
   stdenv,
-  fetchgit,
 }:
 let
-  filename = "vadim-sherbakov-NQSWvyVRIJk-unsplash.jpg";
-  path = "Pictures/wallpapers/${filename}";
+  filename = "wallpaper.jpg";
 in
 stdenv.mkDerivation {
   name = "wallpaper";
-  src = fetchgit {
-    url = "https://github.com/simontheoret/chezmoi";
-    sparseCheckout = [ "${path}" ];
-    hash = "sha256-lN68Xi/MyuQtTyya3E+fyG2tkGznrG8s8Q87ayFVg9k";
-  };
-  postInstall = ''
-    mkdir $out
-    ls
-    cp -v ${path} $out/wallpaper.jpg
+  src = ./.;
+  installPhase = ''
+
+    runHook preInstall
+    mkdir -p $out
+    cp -v ${filename} $out/wallpaper.jpg
+    runHook postInstall
+
   '';
 }

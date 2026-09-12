ac51dbc49f0f771addce0cafa9219485e4300dde
Simon Théorêt <simonteoret@hotmail.com>
2026-09-11 23:26:16 -0400

    fix(nix): use unstable helix instead of building it

--- modules/helix.nix
+++ modules/helix.nix
@@ -1,12 +1,13 @@
 {
   config,
   lib,
-  pkgs,
-  inputs,
+  # pkgs,
+  pkgsUnstable,
+  # inputs,
   ...
 }:
 let
-  helix-master = inputs.helix-master;
+  # helix-master = inputs.helix-master;
   inherit (lib) mkOption mkIf;
   cfg = config.myHelix;
 in
@@ -22,9 +23,9 @@ in
 
   config = mkIf cfg.enable {
     environment.systemPackages = [
-      # pkgsUnstable.helix
+      pkgsUnstable.helix
       # helix-master.helix.packages.
-      helix-master.packages.${pkgs.system}.default
+      # helix-master.packages.${pkgs.system}.default
     ];
   };
 }

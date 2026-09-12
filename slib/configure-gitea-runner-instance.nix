677bef6f00d8fedf0ce1e80b2d304dc8bd2c1569
Simon Théorêt <simonteoret@hotmail.com>
2026-09-12 00:07:44 -0400

    refactor(nix): lib -> slib

---
+++ slib/configure-gitea-runner-instance.nix
@@ -0,0 +1,12 @@
+{
+  name,
+  token ? null,
+  serverUrl ? "http://192.168.18.15:3000",
+}:
+{
+  services.gitea-action-runner.instances.${name}.enable = true;
+  services.gitea-action-runner.instances.name.name = "${name}";
+  services.gitea-action-runner.instances.token.token = "${token}";
+  services.gitea-action-runner.instances.token.url = "${serverUrl}";
+
+}
---
+++ slib/default.nix
@@ -0,0 +1,4 @@
+{ }:
+{
+  configure-gitea-runner-instance = attrs: import ./configure-gitea-runner-instance.nix attrs;
+}

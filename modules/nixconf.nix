0bd6f77ee7c43985970321407ab55ea8eecd36fc
Simon Théorêt <simonteoret@hotmail.com>
2026-09-23 19:23:37 -0400

    fix(nix): update cuda cache on cachix

--- modules/nixconf.nix
+++ modules/nixconf.nix
@@ -25,7 +25,7 @@ in
   config = {
     nix.settings.substituters = [
       "https://nix-community.cachix.org"
-      "https://cache.nixos.org"
+      "https://cache.nixos-cuda.org"
       "https://cuda-maintainers.cachix.org"
       "https://hyprland.cachix.org"
     ];
@@ -33,7 +33,7 @@ in
     nix.settings.trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-      "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
+      "cache.nixos-cuda.org:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M="
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
     ];
 

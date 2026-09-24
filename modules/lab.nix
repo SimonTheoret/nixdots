f0139f34098f114ba2ec340cd50d34e9b7384445
Simon Théorêt <simonteoret@hotmail.com>
2026-09-24 01:26:55 -0400

    Revert "fix(nix): use gitea DOMAIN instead of SSH_DOMAIN"
    This reverts commit 52c4163cba2be5a106aafd48b1717019cf326cce.

--- modules/lab.nix
+++ modules/lab.nix
@@ -107,7 +107,7 @@ in
       };
       settings = {
         server = {
-          DOMAIN = if serverName != null then "${serverName}" else "localhost";
+          SSH_DOMAIN = if serverName != null then "${serverName}" else "localhost";
         };
       };
     };

3fd863a5f6c5953f3bef54e34e3064d74934e90a
Simon Théorêt <simonteoret@hotmail.com>
2026-09-24 00:27:35 -0400

    fix(nix): correct incorret attribute in gitea

--- modules/lab.nix
+++ modules/lab.nix
@@ -107,7 +107,7 @@ in
       };
       settings.server = {
         HTTP_PORT = 3000;
-        server.DOMAIN = if serverName != null then "${serverName}" else "localhost";
+        DOMAIN = if serverName != null then "${serverName}" else "localhost";
       };
     };
     networking.firewall.allowedTCPPorts = [ ] ++ pkgs.lib.optionals (cfg.gitea) [ 3000 ];
